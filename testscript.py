3890706fe48b15ee9e7f0daf9c2dfafe6c67e5f6
BigBoy <>
2022-11-30 09:10:21 +0100

    initial commit testscript

---
+++ testscript.py
@@ -0,0 +1,39 @@
+from models import HackaConv, HackaConvLSTM, HackConvPretraining #TODO, to change to namespace specific import before git
+import datamodules
+import pytorch_lightning as pl
+import torch
+
+epochs = 50
+gpus = [0] if torch.cuda.is_available() else []
+batch_size = 12
+
+datapath = 'data/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton'
+score_path = "data/aha_scores.json"
+ckpt = 'data/pretrained_conv1d.ckpt'
+
+dm = datamodules.HackathonDataModule(datapath=datapath, score_path=score_path, batch_size=batch_size)
+dm.prepare_data() #very slow, opti possible?
+dm.setup()
+
+train_loader = dm.train_dataloader()
+val_loader = dm.val_dataloader()
+# x, y = next(iter(train_loader))
+
+model = HackaConv().load_from_checkpoint(checkpoint_path=ckpt, num_layers=3, num_channels=21, kernel_size=3, lr=1e-4, strict=False)
+
+#freeze
+# model.freeze()
+# for param in model.layers[-2].parameters():
+#     param.requires_grad = True
+
+model = HackaConv(num_layers=3, num_channels=21, kernel_size=3, lr=1e-4)
+trainer = pl.Trainer(gpus=gpus, max_epochs=epochs)
+trainer.fit(model, train_loader, val_dataloaders=val_loader)
+
+# model = HackaConv(num_layers=5, num_channels=21, kernel_size=3, lr=1e-5)
+# trainer = pl.Trainer(gpus=gpus, max_epochs=epochs, log_every_n_steps=10)
+# trainer.fit(model, train_loader, val_dataloaders=val_loader)
+
+# model = HackaConvLSTM(lr=1e-5)
+# trainer = pl.Trainer(gpus=gpus, max_epochs=epochs, log_every_n_steps=10)
+# trainer.fit(model, train_loader, val_dataloaders=val_loader)
