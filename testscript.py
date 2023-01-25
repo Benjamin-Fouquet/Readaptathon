f19420acdd67d05dfb522722a7ed632399f48ca6
SarahR <sarah.reynaud@imt-atlantique.fr>
2023-01-25 14:35:25 +0100

    "modif lstm"

--- testscript.py
+++ testscript.py
@@ -3,12 +3,12 @@ import aaha_datamodules
 import pytorch_lightning as pl
 import torch
 
-epochs = 50
+epochs = 1 #50
 gpus = [0] if torch.cuda.is_available() else []
 batch_size = 12
 
-datapath = 'data/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton'
-score_path = "data/aha_scores.json"
+datapath = "/home/reynaudsarah/Documents/Data/hackathon/AHA/derivatives-one-skeleton" #'data/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton'
+score_path = "/home/reynaudsarah/Documents/Data/hackathon/AHA/aha_scores.json" #"data/aha_scores.json"
 ckpt = None
 # ckpt = 'data/pretrained_conv1d.ckpt'
 
@@ -30,14 +30,14 @@ else:
 # for param in model.layers[-2].parameters():
 #     param.requires_grad = True
 
-model = HackaConvNet(num_layers=3, num_channels=21, kernel_size=3, lr=1e-4)
-trainer = pl.Trainer(gpus=gpus, max_epochs=epochs)
-trainer.fit(model, train_loader, val_dataloaders=val_loader)
+# model = HackaConvNet(num_layers=3, num_channels=21, kernel_size=3, lr=1e-4)
+# trainer = pl.Trainer(gpus=gpus, max_epochs=epochs)
+# trainer.fit(model, train_loader, val_dataloaders=val_loader)
 
 # model = HackaConvNet(num_layers=5, num_channels=21, kernel_size=3, lr=1e-5)
 # trainer = pl.Trainer(gpus=gpus, max_epochs=epochs, log_every_n_steps=10)
 # trainer.fit(model, train_loader, val_dataloaders=val_loader)
 
-# model = HackaConvLSTMNet(lr=1e-5)
-# trainer = pl.Trainer(gpus=gpus, max_epochs=epochs, log_every_n_steps=10)
-# trainer.fit(model, train_loader, val_dataloaders=val_loader)
+model = HackaConvLSTMNet(lr=1e-5)
+trainer = pl.Trainer(gpus=gpus, max_epochs=epochs, log_every_n_steps=10)
+trainer.fit(model, train_loader, val_dataloaders=val_loader)
