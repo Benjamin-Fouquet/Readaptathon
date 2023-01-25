57eb6cef3c78a5f3415c139d857921c3ed48eab2
Claire Scavinner--Dorval <73347729+cScavinner@users.noreply.github.com>
2023-01-25 14:14:05 +0100

    Update main.py

--- models/GraphCNN/main.py
+++ models/GraphCNN/main.py
@@ -28,30 +28,43 @@ import argparse
 home = expanduser("~")
 
 from G_CNN import Model as G_CNN
-from aaha_datamodules import HackathonDataModule
+from datamodules import HackathonDataModule
 
 
 # SET VARIABLES ###############################################################################################
-output_path = home+'/Documents/hackathon/Results/'
-prefix = 'test_01_G_CNN'
-gpu = 0
-num_epochs = 100
-datapath = '/home/claire/Documents/hackathon/AHA/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton/'
-keypoints = [1, 2, 3, 4, 5, 6, 7]
-score_path = "/home/claire/Documents/hackathon/AHA/aha_scores.json"
+parser = argparse.ArgumentParser(description='Dynamic MRI Reconstruction')
+parser.add_argument('-o', '--output_path', help='InOutput path', type=str, required=False, default = home+'/Documents/hackathon/Results/')
+parser.add_argument('-p', '--prefix', help='Prefix', type=str, required=True)
+parser.add_argument('-g', '--gpu', help='gpu to use', type=int, required=False, default = 0)
+parser.add_argument('-n', '--num_epochs', help='Max number of epochs', type=int, required=False, default=100)
+parser.add_argument('-d', '--data_path', help='Data path', type=str, required=False, default='/home/claire/Documents/hackathon/AHA/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton/')
+parser.add_argument('-k', '--keypoints', help='Path to keypoints file', type=list, required=False, default = [1, 2, 3, 4, 5, 6, 7])
+parser.add_argument('-s', '--score_path', help='Path to score file', type=str, required=False, default="/home/claire/Documents/hackathon/AHA/aha_scores.json")
+parser.add_argument('-S', '--strategy', help='Strategy for the adjacence matrix to use', type=str, required=False, default="spatial")
+args = parser.parse_args()
+
+
+#output_path = home+'/Documents/hackathon/Results/'
+#prefix = 'test_01_G_CNN'
+#gpu = 0
+#num_epochs = 100
+#datapath = '/home/claire/Documents/hackathon/AHA/media/rousseau/Seagate5To/Sync-Data/AHA/derivatives-one-skeleton/'
+# keypoints = [1, 2, 3, 4, 5, 6, 7]
+# score_path = "/home/claire/Documents/hackathon/AHA/aha_scores.json"
+# strategy = 'spatial'
 
 # NETWORK #####################################################################################################
 Net = G_CNN(
     criterion = torch.nn.L1Loss(), 
     learning_rate = 1e-4, 
     optimizer = torch.optim.Adam, 
-    prefix = prefix, 
     gpu = 0, 
-    in_channels = 1
+    in_channels = 1,
+    strategy = args.strategy
     )
 
 # DATASET ####################################################################################################
-datamodule = HackathonDataModule(datapath, score_path, keypoints, 1)
+datamodule = HackathonDataModule(args.data_path, args.score_path, args.keypoints, 1)
 datamodule.prepare_data()
 datamodule.setup()
 
@@ -60,13 +73,13 @@ val_loader = datamodule.val_dataloader()
 
 
 # TRAINING ###################################################################################################
-checkpoint_callback = ModelCheckpoint(filepath=output_path+'Checkpoint_'+prefix+'_{epoch}-{val_loss:.2f}')#, save_top_k=1, monitor=)
+checkpoint_callback = ModelCheckpoint(filepath=args.output_path+'Checkpoint_'+args.prefix+'_{epoch}-{val_loss:.2f}')#, save_top_k=1, monitor=)
 
-logger = TensorBoardLogger(save_dir = output_path, name = 'Test_logger',version=prefix)
+logger = TensorBoardLogger(save_dir = args.output_path, name = 'Test_logger',version=args.prefix)
 
 trainer = pl.Trainer(
-    gpus=[gpu],
-    max_epochs=num_epochs,
+    gpus=[args.gpu],
+    max_epochs=args.num_epochs,
     progress_bar_refresh_rate=20,
     logger=logger,
     checkpoint_callback= checkpoint_callback,
@@ -74,12 +87,6 @@ trainer = pl.Trainer(
 )
 
 trainer.fit(Net, train_loader, val_loader)
-torch.save(Net.state_dict(), output_path+prefix+'_torch.pt')
+torch.save(Net.state_dict(), args.output_path+args.prefix+'_torch.pt')
 
 print('Finished Training')
-
-
-# # TESTING ###################################################################################################
-# I=validation_set[0]
-# T=training_set[0]
-# score = Net.forward(I)
